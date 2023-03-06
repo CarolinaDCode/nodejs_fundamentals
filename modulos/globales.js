a6df389ba944a0871e4503cf376e8f6b2f92fb79
diana <dianacarhuamanta03@gmail.com>
2023-03-06 14:32:32 -0500

    globales

--- modulos/globales.js
+++ modulos/globales.js
@@ -2,6 +2,7 @@
  * Una de las funciones muy usadas en Node es setInterval, clearInterval,
  * para evaluar en n tiempo si el servidor está caído o no.
  * TIP: Si no tengo que usar variables globales no usarlas, pues son un foco de problemas
+ * https://static.platzi.com/media/user_upload/global-fa55c760-fa57-43f2-a670-a86345c49c42.jpg
  */
 //Investigar: Concepto de Dependencias Circulares
 //console.log(global);
