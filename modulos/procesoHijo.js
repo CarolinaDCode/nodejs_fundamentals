0415f860d1ff605eb148d9d57eece82db4d6a35a
Diana Carhuamanta <dianacarhuamanta03@gmail.com>
2023-03-10 14:51:15 -0500

    procesoHijo

--- modulos/procesoHijo.js
+++ modulos/procesoHijo.js
@@ -31,6 +31,19 @@ exec('node consola.js', (err, stdout, sterr) =>{
 
 let proceso = spawn('ls', ['-la']);
 
-console.log(proceso)
+//console.log(proceso.pid);
+//console.log(proceso.connected);
+
+proceso.stdout.on('data', function(dato){
+    console.log('¿Proceso esta muerto?')
+    console.log(process.killed)
+    console.log(dato.toString())
+})
+
+proceso.on('exit', function(){
+    console.log('El proceso termino')
+    console.log(process.killed)
+})
+
 
 //FALTA --
